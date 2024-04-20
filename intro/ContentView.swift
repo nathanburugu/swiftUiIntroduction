b5607ac87d28bbbcf0026f33b72384a311f0cb2e
nathanburugu <nate@Nathans-MacBook-Air.local>
2024-04-20 08:02:23 +0300

    introduction to macos

---
+++ intro/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  intro
+//
+//  Created by Nathan Kamau on 20/04/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, worldy!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ intro/introApp.swift
@@ -0,0 +1,17 @@
+//
+//  introApp.swift
+//  intro
+//
+//  Created by Nathan Kamau on 20/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct introApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
